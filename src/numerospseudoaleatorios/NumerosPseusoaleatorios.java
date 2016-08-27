b79374fd65271ec489293878d7390cf1058f43ac
edgarcastro <edgarcastro@DESKTOP-685G4NF>
2016-08-27 17:27:42 -0500

    Se creo metodo de Fibonacci.

--- src/numerospseudoaleatorios/NumerosPseusoaleatorios.java
+++ src/numerospseudoaleatorios/NumerosPseusoaleatorios.java
@@ -8,6 +8,7 @@ package numerospseudoaleatorios;
 import numerospseudoaleatorios.modelo.CongruencialCuadratico;
 import numerospseudoaleatorios.modelo.CongruencialMixto;
 import numerospseudoaleatorios.modelo.CongruencialMultiplicativo;
+import numerospseudoaleatorios.modelo.Fibonacci;
 import numerospseudoaleatorios.modelo.Generador;
 
 /**
@@ -20,9 +21,11 @@ public class NumerosPseusoaleatorios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Generador g = new CongruencialMixto(24, 13, 23, 3);
-        //Generador g = new CongruencialMultiplicativo(15, 101, 6);
-        Generador g =  new CongruencialCuadratico(26, 27, 27, 8, 13);
+        Generador g;
+        //g = new CongruencialMixto(24, 13, 23, 3);
+        //g = new CongruencialMultiplicativo(15, 101, 6);
+        //g =  new CongruencialCuadratico(26, 27, 27, 8, 13);
+        g = new Fibonacci(4, 11, 13, 1, 2, Fibonacci.PRODUCTO);
         System.out.println(""+g.generar(28).toString());
     }
     
---
+++ src/numerospseudoaleatorios/modelo/Fibonacci.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerospseudoaleatorios.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author edgarcastro
+ */
+public class Fibonacci implements Generador{
+    private Integer n0;
+    private Integer n1;
+    private Integer m;
+    private Integer s;
+    private Integer r;
+    private Integer operacion;
+    
+    public static Integer SUMA = 1;
+    public static Integer RESTA = 2;
+    public static Integer PRODUCTO = 3;
+    public static Integer DIVISION = 4;
+
+    public Fibonacci(Integer N0, Integer N1, Integer m, Integer s, Integer r, Integer operacion) {
+        this.n0 = N0;
+        this.n1 = N1;
+        this.m = m;
+        this.s = s;
+        this.r = r;
+        this.operacion = operacion;
+    }
+    
+    
+
+    @Override
+    public List<Integer> generar(Integer cantidad) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(this.n0);
+        numeros.add(this.n1);
+        switch (operacion) {
+            case 1:
+                for (int i = numeros.size(); i < cantidad; i++) {
+                numeros.add((numeros.get(i-this.s) + numeros.get(i-this.r)) % this.m);
+                }
+                break;
+            case 2:
+                for (int i = numeros.size(); i < cantidad; i++) {
+                numeros.add((numeros.get(i-this.s) - numeros.get(i-this.r)) % this.m);
+                }
+                break;
+            case 3:
+                for (int i = numeros.size(); i < cantidad; i++) {
+                numeros.add((numeros.get(i-this.s) * numeros.get(i-this.r)) % this.m);
+                }
+                break;
+            case 4:
+                for (int i = numeros.size(); i < cantidad; i++) {
+                numeros.add((numeros.get(i-this.s) / numeros.get(i-this.r)) % this.m);
+                }
+                break;
+        }
+        return numeros;
+    }
+    
+}
