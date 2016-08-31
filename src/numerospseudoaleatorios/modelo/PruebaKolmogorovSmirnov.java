a266dfd556e8758018e75164b344bf393ae69c76
edgarcastro <edgarcastro@DESKTOP-685G4NF>
2016-08-30 19:18:23 -0500

    Se arreglo fallas en la prueba de Kolmogorov-Smirnov.

--- src/numerospseudoaleatorios/modelo/PruebaKolmogorovSmirnov.java
+++ src/numerospseudoaleatorios/modelo/PruebaKolmogorovSmirnov.java
@@ -19,13 +19,12 @@ public class PruebaKolmogorovSmirnov implements Prueba{
     public Boolean probarNumeros(List numeros) {
         Boolean aceptado = false;
         Collections.sort(numeros);
-        System.out.println("odernados: "+numeros.toString());
         List<Double> Fn = new ArrayList<>();
         Double Xn;
-        int Xi;
+        Double Xi;
         for (int i = 0; i < numeros.size(); i++) {
             Xn = (double)(i+1)/numeros.size();
-            Xi = (int) numeros.get(i);
+            Xi = (double) numeros.get(i);
             Fn.add((double)Math.abs(Xn-Xi));
         }
         Double Dn = Fn.get(0);
@@ -33,8 +32,6 @@ public class PruebaKolmogorovSmirnov implements Prueba{
             if(Fn.get(i) > Dn)
                 Dn = Fn.get(i);
         }
-        System.out.println("Fn = "+Fn.toString());
-        System.out.println("Dn = "+Dn);
         if(Dn < (1.36/Math.sqrt((double)numeros.size()))){
             aceptado = true;
         }
